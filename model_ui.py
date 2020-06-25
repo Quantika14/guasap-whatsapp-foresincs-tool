d91ce1d37bb5d36577fb193868f607df5accea49
manuel <manuelpuertogutierrez@.com>
2020-06-25 10:06:16 +0200

    borrado archivos innecesarios

--- model_ui.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file '.\new_modelo.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
-
